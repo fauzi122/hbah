cbcf9f9d1c8ac8e98b7875b853fd8519a18f2316
fauzi ahmad <ozanfauzi39@gmail.com>
2024-09-10 16:03:21 +0700

    login

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="pull-right hidden-xs">
       {{--  <b>MY-SEALS</b> 0.1  --}}
     </div>
-    <strong>Copyright &copy; 2021 <a href="">Yayasan Mutiara Al-Quran Indonesa</a>.</strong> All rights
+    <strong>Copyright &copy; 2021 <a href="">Yayasan FADILAH ILMI PRATAMA</a>.</strong> All rights
     reserved.
   </footer>
 
